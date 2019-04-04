e934cc6e27056fa10b523529b6d077b634e622ec
manticarodrigo <manticarodrigo@gmail.com>
2019-04-03 23:37:38 -0400

    Added room type selection.

--- src/containers/OnboardingPage/OnboardingPage.tsx
+++ src/containers/OnboardingPage/OnboardingPage.tsx
@@ -1,41 +1,58 @@
-import React, { Component, Fragment } from 'react';
+import React, { ChangeEvent, Component } from 'react';
 import './OnboardingPage.scss';
 
 import OnboardingStepsComponent from './OnboardingStepsComponent/OnboardingStepsComponent';
 
-class OnboardingPage extends Component {
+interface OnboardingPageState {
+  step: number;
+  form: OnboardingForm;
+}
+
+export interface OnboardingForm {
+  type: string;
+}
+
+class OnboardingPage extends Component<any, OnboardingPageState> {
   state = {
     step: 0,
+    form: {
+      type: '',
+    },
   };
 
   handleStart = () => this.setState({ step: 1 });
 
-  handleHandleBack = () => this.setState({ step: this.state.step - 1 });
+  handleBackClicked = () => this.setState({ step: this.state.step - 1 });
+
+  handleTypeClicked = (e: ChangeEvent<HTMLInputElement>) => {
+    this.setState({ form: { ...this.state.form, type: e.target.name }, step: 2 });
+  }
 
   render() {
     const { step } = this.state;
     return (
-      <Fragment>
-        <main className="onboarding__container">
-          <div className="onboarding__container__inner">
-            {step === 0 && (
-              <div className="onboarding__welcome">
-                <h1 className="onboarding__welcome__headline">WELCOME TO<br />THE MAN HOME</h1>
-                <p className="onboarding__welcome__subheadline">Time to like where you wake up</p>
-                <button
-                  className="onboarding__welcome__button"
-                  onClick={this.handleStart}
-                >
-                  START YOUR PROJECT
-                </button>
-              </div>
-            )}
-            {step > 0 && (
-              <OnboardingStepsComponent step={step} handleBackClicked={this.handleHandleBack} />
-            )}
-          </div>
-        </main>
-      </Fragment>
+      <main className="onboarding__container">
+        <div className="onboarding__container__inner">
+          {step === 0 && (
+            <div className="onboarding__welcome">
+              <h1 className="onboarding__welcome__headline">WELCOME TO<br />THE MAN HOME</h1>
+              <p className="onboarding__welcome__subheadline">Time to like where you wake up</p>
+              <button
+                className="onboarding__welcome__button"
+                onClick={this.handleStart}
+              >
+                START YOUR PROJECT
+              </button>
+            </div>
+          )}
+          {step > 0 && (
+            <OnboardingStepsComponent
+              {...this.state}
+              handleBackClicked={this.handleBackClicked}
+              handleTypeClicked={this.handleTypeClicked} />
+          )}
+        </div>
+      </main>
     );
   }
 }
--- src/containers/OnboardingPage/OnboardingStepsComponent/OnboardingStepsComponent.tsx
+++ src/containers/OnboardingPage/OnboardingStepsComponent/OnboardingStepsComponent.tsx
@@ -1,11 +1,15 @@
-import React from 'react';
+import React, { ChangeEvent } from 'react';
 import './OnboardingStepsComponent.scss';
 
 import leftArrow from '../../../assets/images/icons/left-arrow.png';
 
+import { OnboardingForm } from '../OnboardingPage';
+
 interface OnboardingStepsComponentProps {
   step: number;
+  form: OnboardingForm;
   handleBackClicked: () => void;
+  handleTypeClicked: (e: ChangeEvent<HTMLInputElement>) => void;
 }
 
 interface StepType {
@@ -24,6 +28,38 @@ const steps: StepType[] = [
   },
 ];
 
+interface RoomType {
+  type: string;
+  title: string;
+}
+
+const rooms: RoomType[] = [
+  {
+    type: 'LIVING_ROOM',
+    title: 'Living Room',
+  },
+  {
+    type: 'BEDROOM',
+    title: 'Bedroom',
+  },
+  {
+    type: 'DINING_ROOM',
+    title: 'Dining Room',
+  },
+  {
+    type: 'HOME_OFFICE',
+    title: 'Home Office',
+  },
+  {
+    type: 'STUDIO',
+    title: 'Studio',
+  },
+  {
+    type: 'MULTIPURPOSE_ROOM',
+    title: 'Multipurpose Room',
+  },
+];
+
 const OnboardingStepsComponent = (props: OnboardingStepsComponentProps) => {
   const { step, handleBackClicked } = props;
 
@@ -32,7 +68,7 @@ const OnboardingStepsComponent = (props: OnboardingStepsComponentProps) => {
       <div className="onboarding__steps__header">
         <div className="onboarding__steps__header__title">
           <h1 className="u-margin-hug--vert u-text-bold">
-            {steps[step].title}
+            {steps[step - 1].title}
           </h1>
         </div>
         <nav className="onboarding__steps__header__nav">
@@ -41,7 +77,7 @@ const OnboardingStepsComponent = (props: OnboardingStepsComponentProps) => {
         </nav>
       </div>
       <div className="onboarding__steps__subheader">
-        {steps[step].subtitle}
+        {steps[step - 1].subtitle}
       </div>
       <div className="onboarding__steps__content">
         <OnboardingCurrentStepComponent {...props} />
@@ -53,19 +89,43 @@ const OnboardingStepsComponent = (props: OnboardingStepsComponentProps) => {
 const OnboardingCurrentStepComponent = (props: OnboardingStepsComponentProps) => {
   switch (props.step) {
     case 1:
+      return <OnboardingRoomSelectComponent {...props} />;
+    case 2:
+    case 3:
+    case 4:
       return <OnboardingQuizComponent {...props} />;
     default:
       return null;
   }
 };
 
+const OnboardingRoomSelectComponent = (props: OnboardingStepsComponentProps) => (
+  <div className="onboarding__steps__rooms">
+    {rooms.map((room) => (
+      <div className="onboarding__steps__rooms__container" key={room.type}>
+        <input
+          type="radio"
+          id={room.type}
+          name={room.type}
+          checked={props.form.type === room.type}
+          onChange={props.handleTypeClicked}
+        />
+        <label className="onboarding__steps__rooms__item" htmlFor={room.type}>
+          <img src={require(`../../../assets/images/onboarding/rooms/${room.type}.png`)} />
+          <p className="u-margin-hug--bottom">{room.title}</p>
+        </label>
+      </div>
+    ))}
+  </div>
+);
+
 const OnboardingQuizComponent = (props: OnboardingStepsComponentProps) => (
   <div className="onboarding__steps__quiz">
     <div className="onboarding__steps__quiz__image">
-      <img src={require(`../../../assets/images/onboarding/quiz_${props.step}_1.jpg`)} />
+      <img src={require(`../../../assets/images/onboarding/styles/quiz_${props.step - 1}_1.jpg`)} />
     </div>
     <div className="onboarding__steps__quiz__image">
-      <img src={require(`../../../assets/images/onboarding/quiz_${props.step}_2.jpg`)} />
+      <img src={require(`../../../assets/images/onboarding/styles/quiz_${props.step - 1}_2.jpg`)} />
     </div>
   </div>
 );
