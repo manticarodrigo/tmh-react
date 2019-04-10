575f8a97293eb8ea4ca09fc2b419716aaac3952f
manticarodrigo <manticarodrigo@gmail.com>
2019-04-09 20:01:18 -0400

    Moved splash styles to loading component.

--- src/components/LoadingComponent/LoadingComponent.tsx
+++ src/components/LoadingComponent/LoadingComponent.tsx
@@ -4,8 +4,8 @@ import './LoadingComponent.scss';
 import logo from '../../assets/logo.png';
 
 const LoadingComponent = () => (
-  <main className="splash">
-    <div className="splash__container">
+  <main className="loading">
+    <div className="loading__container">
       <div className="loading__content">
         <h1><img className="loading__image" src={logo}/></h1>
         <p className="loading__status">loading...</p>
