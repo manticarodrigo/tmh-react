ef3c953502d66e23a61ae4d286c7edf5a48d42b7
manticarodrigo <manticarodrigo@gmail.com>
2019-05-04 17:48:17 -0400

    Added CollabChat scrollToBottom and collapsed state.

--- src/components/CollabChat/CollabChat.tsx
+++ src/components/CollabChat/CollabChat.tsx
@@ -1,4 +1,4 @@
-import React, { ChangeEvent, FormEvent, memo, useState } from 'react';
+import React, { ChangeEvent, FormEvent, memo, useEffect, useState } from 'react';
 import './CollabChat.scss';
 
 import { CurrentAuth } from '../../store/reducers/AuthReducer';
@@ -15,6 +15,8 @@ const CollabChat = (props: CollabChatProps) => {
   const { auth, project } = props;
   const { socket, messages } = useChatSocket(auth);
   const [message, setMessage] = useState('');
+  const [isCollapsed, setIsCollapsed] = useState(false);
+  const [messageListRef, setMessageListRef] = useState();
 
   const handleMessageChanged = (e: ChangeEvent<HTMLInputElement>) => setMessage(e.target.value);
 
@@ -31,24 +33,41 @@ const CollabChat = (props: CollabChatProps) => {
     }
   };
 
+  const handleToggleButton = () => setIsCollapsed(!isCollapsed);
+
+  const scrollToBottom = () => {
+    if (messageListRef) {
+      const chat = messageListRef;
+      const scrollHeight = chat.scrollHeight;
+      const height = chat.clientHeight;
+      const maxScrollTop = scrollHeight - height;
+      chat.scrollTop = maxScrollTop > 0 ? maxScrollTop : 0;
+    }
+  };
+
+  useEffect(() => scrollToBottom(), [messages]);
+
   return (
     <footer>
-      <aside className="chat">
+      <aside className={`chat${isCollapsed ? ' chat--collapsed' : ''}`}>
         <div className="chat__container">
           <div className="chat__header">
-            <div className="chat__header__title">
+            <div className="chat__header__image">
               <img src={require('../../assets/images/utility/chat.png')} />
-              <h4 className="u-margin-hug--vert">Chat</h4>
             </div>
-            <button className="chat__header__button" />
+            <button className="chat__header__button" onClick={handleToggleButton} />
           </div>
-          <div className="chat__messages">
-            {messages.map((msg, index) => (
-              <p className="u-margin-hug--vert" key={index}>{msg.content}</p>
-            ))}
+          <div className="chat__messages" ref={(ref) => setMessageListRef(ref)}>
+            {messages && (
+              <ul className="u-list-unstyled u-margin-hug--vert">
+                {messages.map((msg, index) => (
+                  <li className="u-margin-hug--vert" key={index}>{msg.content}</li>
+                ))}
+              </ul>
+            )}
           </div>
           <form className="chat__form" onSubmit={handleSendMessage}>
-            <button className="chat__form__attachment" />
+            <button className="chat__form__button chat__form__attachment" />
             <input
               className="chat__form__input"
               type="text"
@@ -56,7 +75,7 @@ const CollabChat = (props: CollabChatProps) => {
               value={message}
               onChange={handleMessageChanged}
             />
-            <button className="chat__form__submit" type="submit">Send</button>
+            <button className="chat__form__button chat__form__submit" type="submit" />
           </form>
         </div>
       </aside>
